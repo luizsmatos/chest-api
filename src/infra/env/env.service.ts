79e854f7010598f053c578ca6faf0966bcf92185
Luiz Gustavo <gusstavopdm@gmail.com>
2023-11-27 15:36:24 -0300

    📦 chore(env): add EnvModule to handle environment variables

---
+++ src/infra/env/env.service.ts
@@ -0,0 +1,10 @@
+import { ConfigService } from '@nestjs/config'
+import { Env } from './env'
+
+export class EnvService {
+  constructor(private configService: ConfigService<Env, true>) {}
+
+  get<T extends keyof Env>(key: T) {
+    return this.configService.get(key, { infer: true })
+  }
+}
