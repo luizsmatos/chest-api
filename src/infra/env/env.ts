e303eb5da045ba066b08347eb0d9cefb0e6e3892
Luiz Gustavo <gusstavopdm@gmail.com>
2023-12-05 16:45:22 -0300

    🔒 chore(env.ts): add JWT_SECRET_KEY to the environment schema to ensure it is provided and properly validated

--- src/infra/env/env.ts
+++ src/infra/env/env.ts
@@ -2,6 +2,7 @@ import { z } from 'zod'
 
 export const envSchema = z.object({
   PORT: z.coerce.number().optional().default(3333),
+  JWT_SECRET_KEY: z.string(),
 })
 
 export type Env = z.infer<typeof envSchema>
