87ecf147b18f95fbf9028b0efd486b210c39db46
Luiz Gustavo <gusstavopdm@gmail.com>
2023-12-05 18:27:33 -0300

    ✨ feat(faker-encrypter.ts): add FakerEncrypter class to provide a fake implementation of the Encrypter interface for testing purposes

---
+++ test/cryptography/faker-encrypter.ts
@@ -0,0 +1,7 @@
+import { Encrypter } from '@/domain/users/cryptography/encrypter'
+
+export class FakerEncrypter implements Encrypter {
+  async encrypt(payload: Record<string, unknown>): Promise<string> {
+    return JSON.stringify(payload)
+  }
+}
