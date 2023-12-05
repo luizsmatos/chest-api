c819649e54933b6bfcda6de3e572d8e05becdff1
Luiz Gustavo <gusstavopdm@gmail.com>
2023-12-05 18:19:37 -0300

    ✨ feat(user.ts): add User entity

---
+++ src/domain/users/entities/user.ts
@@ -0,0 +1,64 @@
+import { Entity } from '@/core/entities/entity'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { Optional } from '@/core/types/optional'
+
+export interface UserProps {
+  name: string
+  email: string
+  password: string
+  createdAt: Date
+  updatedAt?: Date | null
+}
+
+export class User extends Entity<UserProps> {
+  get name() {
+    return this.props.name
+  }
+
+  set name(name: string) {
+    this.props.name = name
+    this.touch()
+  }
+
+  get email() {
+    return this.props.email
+  }
+
+  set email(email: string) {
+    this.props.email = email
+    this.touch()
+  }
+
+  get password() {
+    return this.props.password
+  }
+
+  set password(password: string) {
+    this.props.password = password
+    this.touch()
+  }
+
+  get createdAt() {
+    return this.props.createdAt
+  }
+
+  get updatedAt() {
+    return this.props.updatedAt
+  }
+
+  private touch() {
+    this.props.updatedAt = new Date()
+  }
+
+  static create(props: Optional<UserProps, 'createdAt'>, id?: UniqueEntityID) {
+    const user = new User(
+      {
+        ...props,
+        createdAt: props.createdAt ?? new Date(),
+      },
+      id,
+    )
+
+    return user
+  }
+}
